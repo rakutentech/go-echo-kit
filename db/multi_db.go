7fa616a4980dbca7ecad06acf73fd29684724f52
hehe <hehebu33@gmail.com>
2021-01-19 11:46:57 +0800

    support multi db source connections

---
+++ db/multi_db.go
@@ -0,0 +1,99 @@
+package db
+
+import (
+	"github.com/rakutentech/go-echo-kit/config"
+	"github.com/rakutentech/go-echo-kit/logger"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+	"gorm.io/plugin/dbresolver"
+
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+var onceMultiDb sync.Once
+var singletonDB *gorm.DB
+
+type multiDbConf[] struct {
+	Master string // master db dsn
+	Slave  []string // slave db dsn
+	Name   string // db name
+}
+
+// return singleton DB
+func ConnDB(conf multiDbConf) *gorm.DB {
+	if len(conf) == 0 {
+		logger.LogCritf("[Fatal Error]can not connect to DB: empty dsn given")
+	}
+
+	onceMultiDb.Do(func() {
+		cfg := config.New()
+		env := cfg.GetString("app.env")
+
+		gormConfig := &gorm.Config{}
+
+		// print Slow SQL and happening errors
+		if env != "prod" && env != "pre" {
+			sqlLogger := gormlogger.New(
+				log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+				gormlogger.Config{
+					SlowThreshold: time.Second,   	  // Slow SQL threshold
+					LogLevel:      gormlogger.Error, // Log level
+					Colorful:      false,           // Disable color
+				},
+			)
+			gormConfig = &gorm.Config{
+				Logger: sqlLogger,
+			}
+		}
+
+		/** default DB connection **/
+		defaultMaster := conf[0].Master
+		defaultDbName := conf[0].Name
+		DB, err := gorm.Open(mysql.Open(defaultMaster), gormConfig)
+
+		if err != nil {
+			logger.LogCritf("[Fatal Error]can not init default master DB: %v;dsn: %v", err, defaultMaster)
+		}
+
+		var defaultDialector []gorm.Dialector
+		for _, defaultSlave := range conf[0].Slave {
+			defaultDialector = append(defaultDialector, mysql.Open(defaultSlave))
+		}
+		dbResolver := dbresolver.Register(dbresolver.Config{
+			Replicas: defaultDialector,
+		}, defaultDbName)
+
+		/** default DB connection end **/
+
+		/** connect from other sources **/
+		for idx, c := range conf {
+			if idx == 0 {
+				continue
+			}
+
+			var dialector []gorm.Dialector
+			for _, slave := range c.Slave {
+				dialector = append(dialector, mysql.Open(slave))
+			}
+
+			dbResolver.Register(dbresolver.Config{
+				Sources:  []gorm.Dialector{mysql.Open(c.Master)},
+				Replicas: dialector,
+			}, c.Name)
+		}
+
+		err = DB.Use(dbResolver)
+
+		if err != nil {
+			logger.LogCritf("[Fatal Error]can not connect to DB: %v", err)
+		}
+		singletonDB = DB
+	})
+
+	return singletonDB
+}
--- go.mod
+++ go.mod
@@ -16,4 +16,7 @@ require (
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	gopkg.in/go-playground/validator.v9 v9.31.0
+    gorm.io/driver/mysql v1.0.3
+    gorm.io/gorm v1.20.5
+    gorm.io/plugin/dbresolver v1.0.1
 )
